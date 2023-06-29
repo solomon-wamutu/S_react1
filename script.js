e74c14f0bda19ac25df75c14ca04a4a0fb664e82
solomon-wamutu <solomonwamutu16@gmail.com>
2023-06-29 23:34:04 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function load() {
+    
+var d;
+var b;
+var ans = document.querySelector("div");
+var h = document.querySelector("h1");
+          h.addEventListener("click", () => {
+            Add(7,4)
+           });
+
+        function Add(d,b){
+            return(d+b);
+
+        }
+        console.log(Add(7,4));
+
+    }
